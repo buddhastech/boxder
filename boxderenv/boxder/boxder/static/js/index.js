1defa4760bd6708a7532fcbb72b7fc9c040ad9dd
buddhastech <diegoduarte8343@gmail.com>
2020-07-26 15:40:16 -0600

    Finalización de efecto con javascript y css, modificación de archivos: base.html, index.html, index.js, index.css

--- boxderenv/boxder/boxder/static/js/index.js
+++ boxderenv/boxder/boxder/static/js/index.js
@@ -1,5 +1,12 @@
-var principalText=document.getElementById('principal');
+let principalText = document.getElementById('principal');  // Elemento h2 del main
+let secondText = document.getElementById('second');
+let buttons=document.getElementById('principal-buttons');
 
-principalText.addEventListener('click', () => {
+window.addEventListener('load', () => {
+    
     principalText.classList.add('active');
+    secondText.classList.add('active');
+    buttons.classList.add('active');
+
 });
+
