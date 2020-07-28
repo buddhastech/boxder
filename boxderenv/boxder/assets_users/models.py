f21b8a3f5a1e45240672bf4562af6b0da48c3e20
buddhastech <diegoduarte8343@gmail.com>
2020-07-28 17:58:41 -0600

    Creación de modulos para audit app y assets_users app

--- boxderenv/boxder/assets_users/models.py
+++ boxderenv/boxder/assets_users/models.py
@@ -2,6 +2,43 @@ from django.db import models
 
 # Create your models here.
 
+class Assets(models.Model):
+
+    code = models.CharField(primary_key=True, max_length=8, blank=False, 
+    null=False, unique=True)
+    brand = models.CharField(max_length=255, blank=False, null=False)
+    model = models.CharField(max_length=255, blank=False, null=False)
+    useful_life = models.IntegerField(blank=True, null=True, verbose_name="useful_life")
+    cost = models.DecimalField(max_digits=8, decimal_places=6, blank=False, null=False)
+    weight = models.IntegerField(blank=True, null=True)
+    asset_type = models.CharField(max_length=255, blank=False, null=False, verbose_name="asset_type")
+    admission_date = models.DateField(auto_now=True, verbose_name="admission_date")
+    
+    STATUS_OPTIONS = (
+        ('A', 'activo'),
+        ('B', 'baja'),
+        ('S', 'suspendido')
+    )
+    actual_status = models.CharField(max_length=1, choices=STATUS_OPTIONS, verbose_name="actual_status")
+    provider = models.CharField(max_length=60, blank=False, null=False)
+
+    class Meta:
+
+        db_table = "assets"
+
+
+class Departments(models.Model):
+
+    department_id = models.AutoField(primary_key=True, unique=True, verbose_name="department_id")
+    name = models.CharField(max_length=60, blank=False, null=False)
+    creation_date = models.DateField(auto_now=True, verbose_name="create_date")
+    staff_amount = models.IntegerField(blank=True, null=True, verbose_name="staff_amount")
+
+    class Meta:
+
+        db_table = "departments"
+
+
 class Users(models.Model):
     
     identification_card = models.CharField(primary_key=True, max_length=15, blank=False,
@@ -9,24 +46,45 @@ class Users(models.Model):
     name = models.CharField(max_length=255, blank=False, null=False)
     surnames = models.CharField(max_length=255, blank=False, null=False)
     phone = models.CharField(max_length=14, blank=False, null=False, unique=True)
-    department = models.CharField(max_length=255, blank=False, null=False)
+    department = models.ForeignKey(Departments, on_delete=models.PROTECT)
     age = models.CharField(max_length=2, blank=False, null=False)
     status = models.BooleanField(default=False)
-    admission_date = models.DateField(auto_now=True)
+    admission_date = models.DateField(auto_now=True, verbose_name="admission_date")
     email = models.CharField(max_length=255, unique=True)
 
     class Meta:
 
         db_table = "users"
 
-class Assets(models.Model):
 
-    code = models.CharField(primary_key=True, max_length=8, blank=False, 
-    null=False, unique=True)
-    brand = models.CharField(max_length=255, blank=False, null=False)
-    model = models.CharField(max_length=255, blank=False, null=False)
-    useful_life = models.IntegerField(max_length=2, blank=False, null=False)
-    cost = models.FloatField(max_digits=4,decimal_place=2)
-    # max_digits = total de digitos
-    #decimal_place = total de decimales
-    # 58.979 = max_digits 5 decimal_place = 3
+class AccumulatedMoney(models.Model):
+
+    accumulated_id = models.AutoField(primary_key=True, unique=True, verbose_name="accumulated_id")
+    user_id = models.ForeignKey(Users, on_delete=models.PROTECT)
+    accumulated = models.DecimalField(max_digits=8, decimal_places=6, blank=False, null=False)
+
+    class Meta:
+
+        db_table = "accumulated_money"
+
+
+class Suspensions(models.Model):
+
+    suspension_id = models.AutoField(primary_key=True, unique=True, verbose_name="suspension_id")
+    user_id = models.ForeignKey(Users, on_delete=models.PROTECT)
+    number_of_suspenssions = models.IntegerField(blank=False, null=False)
+
+    class Meta:
+
+        db_table = "suspensions"
+
+
+class Depreciations(models.Model):
+
+    depreciation_code = models.AutoField(primary_key=True, unique=True, verbose_name="depreciation_code")
+    assets_code = models.ForeignKey(Assets, on_delete=models.PROTECT, verbose_name="assets_code")
+    depreciation = models.DecimalField(max_digits=8, decimal_places=6, blank=False, null=False)
+
+    class Meta:
+
+        db_table = "depreciations"
--- boxderenv/boxder/audit/models.py
+++ boxderenv/boxder/audit/models.py
@@ -1,3 +1,71 @@
 from django.db import models
+from django.utils import timezone
+from assets_users.models import Users # import of assets_users app
+from assets_users.models import Assets
 
-# Create your models here.
+class UserRegistration(models.Model):
+
+    registry_id = models.AutoField(primary_key=True, unique=True, verbose_name="registry_id")
+    user_id = models.ForeignKey(Users, on_delete=models.PROTECT)
+    creation_date = models.DateField(auto_now=True)
+    creation_time = models.TimeField(default=timezone.now)
+
+    class Meta:
+
+        db_table = "user_registration"
+        order_with_respect_to = "user_id"
+
+
+class UserModification(models.Model):
+
+    modification_id = models.AutoField(primary_key=True, unique=True, verbose_name="modification_id")
+    user_id = models.ForeignKey(Users, on_delete=models.PROTECT, verbose_name="user_id") # id usuario modificado
+    modifying_id = models.CharField(max_length=15, blank=False, null=False, verbose_name="modifying_id")
+    modification_date = models.DateField(auto_now=True, verbose_name="modification_date")
+    modification_time = models.TimeField(timezone.now)
+    justification = models.CharField(max_length=255, blank=False, null=False)
+
+    class Meta: 
+
+        db_table = "user_modification"
+        order_with_respect_to = "user_id"
+
+class IncomesAndExits(models.Model):
+
+    user_id = models.ForeignKey(Users, on_delete=models.PROTECT, verbose_name="user_id")
+    date_admission = models.DateField(auto_now=True, verbose_name="admission_date", blank=False, null=False)
+    hour_admission = models.TimeField(timezone.now, blank=False, null=False)
+    departure_date = models.DateField(auto_now=True, verbose_name="departure_date", blank=False, null=False)
+    departura_hour = models.TimeField(default=timezone.now)
+
+    class Meta:
+
+        db_table = "incomes_and_exits"
+        order_with_respect_to = "user_id"
+
+
+class UnauthorizedIncome(models.Model):
+
+    user_id = models.ForeignKey(Users, on_delete=models.PROTECT, verbose_name="user_id")
+    try_date = models.DateField(auto_now=True, blank=False, null=False, verbose_name="try_date")
+    try_hour = models.TimeField(default=timezone)
+
+    class Meta:
+
+        db_table = "unauthorized_income"
+        order_with_respect_to = "user_id"
+
+
+class AssetModification(models.Model):
+    
+    modifying_id = models.ForeignKey(Users, on_delete=models.PROTECT, verbose_name="modifying_id")
+    asset_id = models.ForeignKey(Assets, on_delete=models.PROTECT, verbose_name="asset_id")
+    modification_date = models.DateField(auto_now=True, verbose_name="modification_date")
+    modification_time = models.TimeField(timezone.now)
+    justification = models.CharField(max_length=255, blank=False, null=False)
+
+    class Meta:
+        
+        db_table = "asset_modification"
+        order_with_respect_to = "asset_id"
+
